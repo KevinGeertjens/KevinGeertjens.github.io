96aef9c39645cd8984932422f1b08a868c8c1960
KevinGeertjens <K.Geertjens@gmail.com>
2023-06-01 22:55:13 +0200

    Create base react project

---
+++ README.md
@@ -0,0 +1,2 @@
+# Kevin Geertjens Portfolio
+This is the Github page hosting my personal developer portfolio, showcasing my projects
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css"
+
+function App() {
+  return (
+    <div className="App bg-stone-900 text-white overflow-x-hidden overflow-y-auto">
+      <p>Hello World!</p>
+    </div>
+  );
+}
+
+export default App;
